1295b0276e756b9d01fadade9e5b5b267eab251b
Swooshoo <eliotlee1415@gmail.com>
2024-02-18 17:40:51 -0800

    added server.py

---
+++ backend/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='frontend', static_url_path='')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
